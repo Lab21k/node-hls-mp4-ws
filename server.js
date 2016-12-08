c37d2ee30b5ba4175ab1e0af9dba1c238f919f22
Andre Abadesso <andre.abadesso@gmail.com>
2016-12-08 15:23:34 -0200

    feat: added debounce to convert

--- server.js
+++ server.js
@@ -6,42 +6,46 @@ const fs           = require('fs'),
         INPUT_DIR  = process.env.INPUT_DIR || '.',
         OUTPUT_DIR = process.env.OUTPUT_DIR || '.',
         spawn      = require('child_process').spawn,
-        redis       = require('redis');
+        redis      = require('redis'),
+        _          = require('lodash');
 
 const redisClient = redis.createClient();
 
+function convert(filename) {
+    let newFilename = filename.split('.')[0] + '.mp4';
+
+    let ffmpeg = spawn('ffmpeg', [
+        '-i',
+        `${INPUT_DIR}/${filename}`,
+        '-c:v',
+        'libx264',
+        '-preset',
+        'medium',
+        '-tune',
+        'film',
+        '-crf',
+        '23',
+        '-strict',
+        'experimental',
+        '-c:a',
+        'aac',
+        '-b:a',
+        '192k',
+        `${OUTPUT_DIR}/${newFilename}`
+    ]);
+
+    ffmpeg.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        if (code === 0) {
+            redisClient.publish('videofragment', `${newFilename}`);
+            fs.createReadStream(`${OUTPUT_DIR}/${newFilename}`)
+                .pipe(fs.createWriteStream(`${OUTPUT_DIR}/current.mp4`));
+        }
+    });
+}
+
 fs.watch(INPUT_DIR, {}, (eventType, filename) => {
     if (filename.indexOf('.ts') > -1) {
-        console.log('Fragment', filename);
-        let newFilename = filename.split('.')[0] + '.mp4';
-
-        let ffmpeg = spawn('ffmpeg', [
-            '-i',
-            `${INPUT_DIR}/${filename}`,
-            '-c:v',
-            'libx264',
-            '-preset',
-            'medium',
-            '-tune',
-            'film',
-            '-crf',
-            '23',
-            '-strict',
-            'experimental',
-            '-c:a',
-            'aac',
-            '-b:a',
-            '192k',
-            `${OUTPUT_DIR}/${newFilename}`
-        ]);
-
-        ffmpeg.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-            if (code === 0) {
-                redisClient.publish('videofragment', `${newFilename}`);
-                fs.createReadStream(`${OUTPUT_DIR}/${newFilename}`)
-                    .pipe(fs.createWriteStream(`${OUTPUT_DIR}/current.mp4`));
-            }
-        });
+        _.debounce(convert(filename), 1500);
     }
 });
