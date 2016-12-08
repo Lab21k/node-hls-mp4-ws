81936f1143876bd7912727b22ad8a8ccfbcb453d
Andre Abadesso <andre.abadesso@gmail.com>
2016-12-08 15:16:32 -0200

    feat: converting ts to mp4

---
+++ server.js
@@ -0,0 +1,47 @@
+// ffmpeg -i $1.ts -c:v libx264 -preset medium -tune film -crf 23 -strict experimental -c:a aac -b:a 192k output.mp4
+
+'use strict';
+
+const fs           = require('fs'),
+        INPUT_DIR  = process.env.INPUT_DIR || '.',
+        OUTPUT_DIR = process.env.OUTPUT_DIR || '.',
+        spawn      = require('child_process').spawn,
+        redis       = require('redis');
+
+const redisClient = redis.createClient();
+
+fs.watch(INPUT_DIR, {}, (eventType, filename) => {
+    if (filename.indexOf('.ts') > -1) {
+        console.log('Fragment', filename);
+        let newFilename = filename.split('.')[0] + '.mp4';
+
+        let ffmpeg = spawn('ffmpeg', [
+            '-i',
+            `${INPUT_DIR}/${filename}`,
+            '-c:v',
+            'libx264',
+            '-preset',
+            'medium',
+            '-tune',
+            'film',
+            '-crf',
+            '23',
+            '-strict',
+            'experimental',
+            '-c:a',
+            'aac',
+            '-b:a',
+            '192k',
+            `${OUTPUT_DIR}/${newFilename}`
+        ]);
+
+        ffmpeg.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+            if (code === 0) {
+                redisClient.publish('videofragment', `${newFilename}`);
+                fs.createReadStream(`${OUTPUT_DIR}/${newFilename}`)
+                    .pipe(fs.createWriteStream(`${OUTPUT_DIR}/current.mp4`));
+            }
+        });
+    }
+});
